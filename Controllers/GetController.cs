6fe9ad869b7bb775b5a22581430bc108d0885878
Mark Kraus <github@markekraus.com>
2018-01-29 18:13:27 -0600

    repro code

---
+++ Controllers/GetController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace mvc.Controllers
+{
+    [Route("api/[controller]")]
+    public class GetController : Controller
+    {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            Hashtable args = new Hashtable();
+            foreach (var key in Request.Query.Keys)
+            {
+                args.Add(key, String.Join(", ", Request.Query[key]));
+            }
+            Hashtable headers = new Hashtable();
+            foreach (var key in Request.Headers.Keys)
+            {
+                headers.Add(key, String.Join(", ", Request.Headers[key]));
+            }
+            Hashtable output = new Hashtable
+            {
+                {"args"   , args},
+                {"headers", headers},
+                {"origin" , Request.HttpContext.Connection.RemoteIpAddress.ToString()},
+                {"url"    , UriHelper.GetDisplayUrl(Request)},
+                {"method" , Request.Method}
+            };
+
+            if (Request.HasFormContentType)
+            {
+                Hashtable form = new Hashtable();
+                foreach (var key in Request.Form.Keys)
+                {
+                    form.Add(key,Request.Form[key]);
+                }
+                output["form"] = form;
+            }
+
+            string data = new StreamReader(Request.Body).ReadToEnd();
+            if (!String.IsNullOrEmpty(data))
+            {
+                output["data"] = data;
+            }
+
+            return Json(output);
+        }
+    }
+}
---
+++ Controllers/RedirectController..cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Primitives;
+
+namespace RedirectCookie.Controllers
+{
+    [Route("api/[controller]")]
+    public class RedirectController : Controller
+    {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            Response.Cookies.Append("special","test123");
+            Response.StatusCode = 302;
+            string url = Regex.Replace(input: Request.GetDisplayUrl(), pattern: "\\/Redirect.*", replacement: "", options: RegexOptions.IgnoreCase);
+            url = $"{url}/Get/";
+            Response.Headers.Add("Location", url);
+            return Json(null);
+        }
+    }
+}
